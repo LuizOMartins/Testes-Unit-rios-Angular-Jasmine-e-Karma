2baab8411ddc644b8b6835334606cb9434159ded
LuizOMartinsProver <loliveira@provertec.com.br>
2023-06-12 19:09:56 -0300

    Adicionando arquivos de testes

---
+++ curso-angular-testes-unitarios-template-main/src/app/asynchronous-component/asynchronous-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AsynchronousComponentComponent } from './asynchronous-component.component';
+
+describe('AsynchronousComponentComponent', () => {
+  let component: AsynchronousComponentComponent;
+  let fixture: ComponentFixture<AsynchronousComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ AsynchronousComponentComponent ],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AsynchronousComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
